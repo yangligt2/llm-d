f27103027d482e400eb90a6e95adf96b4d2cabd2
Zhengxu Xia <zhengxux@google.com>
2026-01-23 17:21:08 -0800

    Update the setup scripts
    1. Add a customized netowrk in case the default network runs out of IP.
    2. There are projects whose policies do not support EXTERNAL_MANAGED_HTTP_HTTPS.
    Port-forward into the decode pod to allow curl from outside of the
    private network.
    3. Check gke versions.

--- guides/pd-disaggregation/v7x-aio-2-llmd-stack.sh
+++ guides/pd-disaggregation/v7x-aio-2-llmd-stack.sh
@@ -1,9 +1,28 @@
+#!/bin/bash
+
 # Source the env vars
 source v7x-aio-0-env.sh
+
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+if ! gcloud resource-manager org-policies describe \
+  compute.restrictLoadBalancerCreationForTypes \
+  --project=${PROJECT_ID} \
+  --format="value(listPolicy.allowedValues)" | \
+  grep -q "EXTERNAL_MANAGED_HTTP_HTTPS";then
+  gateway_config="../prereq/gateway-provider/common-configurations/gke.yaml"
+  echo -e "${YELLOW}Warning: Project ${PROJECT_ID}'s policy does not support" \
+    "EXTERNAL_MANAGED_HTTP_HTTPS. Fall gatewayClassName in ${gateway_config}" \
+    "from 'gke-l7-regional-external-managed' to 'gke-l7-rilb'." \
+    "Note ${gateway_config} will be modified.${NC}"
+  sed -i 's/gke-l7-regional-external-managed/gke-l7-rilb/' ${gateway_config}
+fi
 
 # Install the Inference Extension CRDs.
 # Although GKE already installed it: https://docs.cloud.google.com/kubernetes-engine/docs/how-to/deploy-gke-inference-gateway#compatibility, EPP will crash if not explicitly install it.
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api-inference-extension/raw/v1.0.0/config/crd/bases/inference.networking.x-k8s.io_inferenceobjectives.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api-inference-extension/raw/v1.2.0-rc.1/config/crd/bases/inference.networking.k8s.io_inferencepools.yaml
 
 # create namespace
 kubectl create namespace ${NAMESPACE}
